f7449ee889180f85a46291af958f703c5a90098d
bradyfontenot <bradyfontenot@gmail.com>
2023-10-21 22:14:35 -0500

    ex1

--- 1-producer-consumer/main.go
+++ 1-producer-consumer/main.go
@@ -10,22 +10,33 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func producer(stream Stream) (tweets []*Tweet) {
+var (
+	ch = make(chan *Tweet)
+)
+
+func producer(stream Stream, wg *sync.WaitGroup) {
+	defer wg.Done()
+
 	for {
 		tweet, err := stream.Next()
+
 		if err == ErrEOF {
-			return tweets
+			close(ch)
+			return
 		}
 
-		tweets = append(tweets, tweet)
+		ch <- tweet
 	}
 }
 
-func consumer(tweets []*Tweet) {
-	for _, t := range tweets {
+func consumer(wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for t := range ch {
 		if t.IsTalkingAboutGo() {
 			fmt.Println(t.Username, "\ttweets about golang")
 		} else {
@@ -37,12 +48,16 @@ func consumer(tweets []*Tweet) {
 func main() {
 	start := time.Now()
 	stream := GetMockStream()
-
-	// Producer
-	tweets := producer(stream)
+	wg := sync.WaitGroup{}
+	wg.Add(2)
 
 	// Consumer
-	consumer(tweets)
+	go consumer(&wg)
 
-	fmt.Printf("Process took %s\n", time.Since(start))
+	// Producer
+	go producer(stream, &wg)
+
+	wg.Wait()
+
+	fmt.Printf("\nProcess took %s\n", time.Since(start))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/loong/go-concurrency-exercises
 
 go 1.19
+
+require golang.org/x/time v0.3.0 // indirect
